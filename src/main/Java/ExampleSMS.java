1aa4ea6da355dde72b557983e81a7ba83c30c77d
Damina Townes <daminatownes@gmail.com>
2018-10-03 17:55:05 -0400

    first commit

---
+++ src/main/Java/ExampleSMS.java
@@ -0,0 +1,23 @@
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+public class ExampleSMS {
+  // Find your Account Sid and Token at twilio.com/user/account
+  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  public static final String AUTH_TOKEN = "your_auth_token";
+
+  public static void main(String[] args) {
+    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+    Message message = Message.creator(new PhoneNumber("+15558675309"),
+        new PhoneNumber("+15017250604"), 
+        "This is the ship that made the Kessel Run in fourteen parsecs?").create();
+    
+    Message message1 = Message.creator(new PhoneNumber("+6312526048"),
+            new PhoneNumber("+6312526048"), 
+            "http://bit.ly/2PXEZnW ?").create();
+
+    System.out.println(message.getSid());
+  }
+}
---
+++ src/main/Java/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author damin
+ *
+ */
+package Java;
