810e8844e965c4e3773ae491ba7d3869181aa690
Damina <33079664+down4dublin@users.noreply.github.com>
2018-10-06 15:21:18 -0400

    Update ExampleSMS.java

--- src/main/Java/ExampleSMS.java
+++ src/main/Java/ExampleSMS.java
@@ -4,19 +4,19 @@ import com.twilio.type.PhoneNumber;
 
 public class ExampleSMS {
   // Find your Account Sid and Token at twilio.com/user/account
-  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  public static final String ACCOUNT_SID = "AC1ba67ccf30ea2fe309901290ea6937e3";
   public static final String AUTH_TOKEN = "your_auth_token";
 
   public static void main(String[] args) {
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-    Message message = Message.creator(new PhoneNumber("+15558675309"),
-        new PhoneNumber("+15017250604"), 
-        "This is the ship that made the Kessel Run in fourteen parsecs?").create();
+    Message message = Message.creator(new PhoneNumber("+16312526048"),
+        new PhoneNumber("+16312526048"), 
+        "Hello from Damina Townes CS 643 Fall 2018").create();
     
     Message message1 = Message.creator(new PhoneNumber("+6312526048"),
             new PhoneNumber("+6312526048"), 
-            "http://bit.ly/2PXEZnW ?").create();
+            "Hello from Damina Townes CS 643 Fall 2018").create();
 
     System.out.println(message.getSid());
   }
